226c8bb8ad544ba77dbf1cc90fe6457c0663e9ca
Max Armino <maxxarmino@protonmail.com>
2018-11-14 14:49:45 +0100

    First commit
    Start of frontend + backend

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res) {
+   res.sendfile(__dirname + '/views/index.html')
+});
+
+app.post("/common/transaction", function(req, res) { 
+  console.log('static file request : ' + req.params);
+  res.sendfile( __dirname + "/views/transaction.html");
+});
+
+app.get("/common/safebtc.css", function(req, res) { 
+  res.sendfile( __dirname + "/views/common/safebtc.css");
+});
+
+app.get("/common/transaction", function(req, res) { 
+  res.send("403");
+});
+
+app.get(/^(.+)$/, function(req, res){ 
+    console.log('static file request : ' + req.params);
+    res.sendfile( __dirname + "/views/common/" + req.params[0]);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
---
+++ views/common/safebtc.js
@@ -0,0 +1,3 @@
+var input = document.getElementById('address');
+input.focus();
+input.select();
